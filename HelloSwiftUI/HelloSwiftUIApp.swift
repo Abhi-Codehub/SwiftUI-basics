d17fe568148f57a9eecd3ddb57570a6e0ff38361
Abhishek Aggarwal <abhishekaggarwal345@gmail.com>
2024-06-24 12:16:21 +0530

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by Abhishek Aggarwal on 24/06/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
